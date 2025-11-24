87492733e1f3f257fe26626afc9a81ee313b375d
Abilash Ravi <abilashravi09@gmail.com>
2025-11-24 09:24:21 +0530

    update

---
+++ src/app/core/home/home.component.ts
@@ -0,0 +1,17 @@
+import { AfterViewInit, Component } from '@angular/core';
+declare function text() :any
+declare function me() :any
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    text();
+    me()
+  }
+
+}
